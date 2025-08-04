3026d507aaa312420bbcbb16b3b624e00169d006
codedjade003 <132352148+codedjade003@users.noreply.github.com>
2025-08-04 23:24:45 +0000

    fixed

--- aiff-website/src/assets/films/index.js
+++ aiff-website/src/assets/films/index.js
@@ -1,12 +1,11 @@
-import matiAZazzau from './Mati-a-Zazzau.png';
-import aLotLikeLove from './A-Lot-Like-Love.png';
-import thePlan from './The-Plan.png';
+import matiAZazzau from './Mati a Zazzau.png';
+import aLotLikeLove from './A Lot Like Love.png';
+import thePlan from './The Plan.png';
 
 const filmImages = {
   'Mati a Zazzau': matiAZazzau,
   'A Lot Like Love': aLotLikeLove,
   'The Plan': thePlan,
 };
-
 
 export default filmImages;
