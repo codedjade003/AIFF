73c8687e761572867cbf2c5b4bedb89f08ea19fc
codedjade003 <132352148+codedjade003@users.noreply.github.com>
2025-08-04 23:28:10 +0000

    fixed again ohh

--- aiff-website/src/assets/films/index.js
+++ aiff-website/src/assets/films/index.js
@@ -1,11 +1,11 @@
-import matiAZazzau from './Mati a Zazzau.png';
-import aLotLikeLove from './A Lot Like Love.png';
-import thePlan from './The Plan.png';
+import matiAZazzau from './mati-a-zazzau.png';
+import aLotLikeLove from './a-lot-like-love.png';
+import thePlan from './the-plan.png';
 
 const filmImages = {
   'Mati a Zazzau': matiAZazzau,
   'A Lot Like Love': aLotLikeLove,
-  'The Plan': thePlan,
+  'The Plan': thePlan
 };
 
 export default filmImages;
